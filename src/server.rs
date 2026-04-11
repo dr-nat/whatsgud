60124b0a050e66359b632bdd987e00b916ca1991
Nathaniel Parah <nathanielparah579@gmail.com>
2026-04-11 17:18:16 +0100

    created a shared state like a room where all connected users stay
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/server.rs
+++ src/server.rs
@@ -2,9 +2,14 @@ use std::{
     sync::{Arc, Mutex},
     thread,
     collections::HashMap,
+    error::Error,
 };
+use tokio_tungstenite::tungstenite::protocol::Message;
+use futures_channel::mpsc::{unbounded, UnboundedSender};
 
-fn server_abs() -> (
-    let shared_state = Arc::new(Mutex::new(HashMap::new()));
-    
-)
+fn server_abs() -> Result<(), Box<dyn Error>> {
+    type Tx = UnboundedSender<Message>;
+    let shared_state: Arc<Mutex<HashMap<String, Tx>>> = Arc::new(Mutex::new(HashMap::new()));
+
+   Ok(()) 
+}
