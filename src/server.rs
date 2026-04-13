eff422b8ce46897f3766e4c02a757345c757617b
Nathaniel Parah <nathanielparah579@gmail.com>
2026-04-13 16:59:32 +0100

    added a new crate
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/server.rs
+++ src/server.rs
@@ -7,9 +7,8 @@ use std::{
 use tokio_tungstenite::tungstenite::protocol::Message;
 use futures_channel::mpsc::{unbounded, UnboundedSender};
 
-fn server_abs() -> Result<(), Box<dyn Error>> {
+fn handle_connection() {
     type Tx = UnboundedSender<Message>;
     let shared_state: Arc<Mutex<HashMap<String, Tx>>> = Arc::new(Mutex::new(HashMap::new()));
 
-   Ok(()) 
 }
