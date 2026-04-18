c6cde8aa7388a03be47416cd4d63c90533b32cfa
Nathaniel Parah <nathanielparah579@gmail.com>
2026-04-18 20:24:31 +0100

    Added the tokio crates for spawning tasks in full version
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/db_manager.rs
+++ src/db_manager.rs
@@ -1,12 +1,15 @@
 use tokio::sync::mpsc;
 use crate::models;
+use std::{
+    fs::
+};
 
 pub async fn db_task(mut msg: mpsc::Receiver<String>) -> mpsc::Sender<String> {
    tokio::spawn(async move { 
        while let Some(rx) = msg.recv.await {
            models::MsgFormat::new(rx)
-
-          } 
+              
+           } 
        }
    )
 }
